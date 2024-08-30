171459fc2692f3adfa59e58a474e941f585350c0
Your Name <you@example.com>
2024-08-30 13:13:21 +0530

    new method added here

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,14 @@ public class HelloWorld {
        System.out.println("Hello Java Method");
      System.out.println("Hello Changed Java Method");
      }
+    static void method2(){
+       System.out.println("Hello Java Method 2");
+     System.out.println("Hello Changed Java Method");
+     }
    
     public static void main(String[] args) {
         System.out.println("Hello Java World");
          method();
+	 method2();
     }
 }
