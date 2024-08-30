d03776816056990cdd730c141450a98f70e3c403
tanvitambe08 <103626271+tanvitambe08@users.noreply.github.com>
2024-08-30 12:49:14 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    static void method(){
+       System.out.println("Hello Java Method");
+     System.out.println("Hello Changed Java Method");
+     }
+   
+    public static void main(String[] args) {
+        System.out.println("Hello Java World");
+         method();
+    }
+}
